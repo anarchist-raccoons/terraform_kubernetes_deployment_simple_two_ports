82443bd41cb4887ece293d29e6838765ed1e652a
Rory McNicholl <rory.mcnicholl@london.ac.uk>
2020-06-19 11:50:51 +0000

    initial commit of templates

---
+++ variables.tf
@@ -0,0 +1,50 @@
+variable "host" { }
+variable "username" { }
+variable "password" { }
+variable "client_certificate" { }
+variable "client_key" { }
+variable "cluster_ca_certificate" { }
+variable "docker_image" { }
+variable "app_name" { }
+variable "primary_port" { }
+variable "secondary_port" { }
+
+variable "env_from" { }
+
+variable "command" {
+  default = []
+}
+
+variable "mount_path" {
+}
+
+variable "image_pull_secrets" {
+}
+
+variable "resource_version" {
+  description = "Unused variable, used to create a dependency sequence."
+  type = "list"
+  default = []
+}
+
+variable "pvc_claim_name" {}
+variable "secondary_pvc_claim_name" {
+  default = ""
+  }
+
+variable "replicas" {
+  default = 1
+}
+
+variable "load_balancer_source_ranges" {
+  type = "list"
+  default = []
+}
+
+variable "service_type" {
+  default = "LoadBalancer"
+}
+
+variable "load_balancer_ip" {
+  default = ""
+}
